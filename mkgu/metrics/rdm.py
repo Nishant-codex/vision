Nishant-codex/vision

commit a2d6e4acb4ba01eb9d6bf4f017e353ca28af4909

fix re-ordering of dimensions

mkgu/metrics/rdm.py
@@ -24,7 +24,8 @@ class RSA(Characterization):
     def __call__(self, assembly):
         correlations = np.corrcoef(assembly) if assembly.dims[-1] == 'neuroid' else np.corrcoef(assembly.T).T
         coords = {coord: coord_value for coord, coord_value in assembly.coords.items() if coord != 'neuroid'}
-        dims = [dim if dim != 'neuroid' else assembly.dims[0] for dim in assembly.dims]
+        dims = [dim if dim != 'neuroid' else assembly.dims[(i - 1) % len(assembly.dims)]
+                for i, dim in enumerate(assembly.dims)]
         return mkgu.assemblies.NeuroidAssembly(correlations, coords=coords, dims=dims)
 
 

tests/test_metrics.py
@@ -20,17 +20,23 @@ from mkgu.metrics.rdm import RSA, RDMMetric
 
 
 def test_hvm_it_rdm():
-    loaded = np.load(os.path.join(os.path.dirname(__file__), "it_rdm.p"), encoding="latin1")
-
     hvm_it_v6_obj = _load_hvm(group=lambda hvm: hvm.multi_groupby(["category", "obj"]))
-
     assert hvm_it_v6_obj.shape == (64, 168)
+    _test_hvm_it_rdm(hvm_it_v6_obj)
 
+
+def test_hvm_it_rdm_T():
+    hvm_it_v6_obj = _load_hvm(group=lambda hvm: hvm.multi_groupby(["category", "obj"])).T
+    assert hvm_it_v6_obj.shape == (168, 64)
+    _test_hvm_it_rdm(hvm_it_v6_obj)
+
+
+def _test_hvm_it_rdm(hvm_it_v6_obj):
+    loaded = np.load(os.path.join(os.path.dirname(__file__), "it_rdm.p"), encoding="latin1")
     rsa_characterization = RSA()
     rsa = rsa_characterization(hvm_it_v6_obj)
-
-    assert isinstance(rsa, mkgu.assemblies.DataAssembly)
-    assert rsa.shape == (64, 64)
+    assert list(rsa.shape) == [64, 64]
+    assert list(rsa.dims) == ['presentation', 'presentation']
     assert rsa.values == approx(loaded, abs=1e-6)
 
 

commit 74d0469146182e43efbde2ad063229958cdd5d90

fix triu indexing of assembly
commit a2d6e4a broke the RDMMetric because RDM returned a DataArray instead of an ndarray and positional tuple-indexing seems to work incorrectly there. Using `.values` for now

mkgu/metrics/rdm.py
@@ -48,7 +48,8 @@ class RDMCorrelationCoefficient(Similarity):
     """
 
     def __call__(self, assembly1, assembly2):
-        corr = np.corrcoef(self._preprocess_assembly(assembly1), self._preprocess_assembly(assembly2))
+        triu1, triu2 = self._preprocess_assembly(assembly1), self._preprocess_assembly(assembly2)
+        corr = np.corrcoef(triu1, triu2)
         assert len(corr.shape) == 2 and corr.shape[0] == 2 and corr.shape[1] == 2
         assert all(np.diag(corr) == 1)
         assert corr[0, 1] == corr[1, 0]
@@ -59,4 +60,4 @@ class RDMCorrelationCoefficient(Similarity):
         assert assembly.shape[0] == assembly.shape[1]
         assert np.allclose(np.diag(assembly), np.zeros((assembly.shape[0],)), atol=1e-10)
         triangular_indices = np.triu_indices(assembly.shape[0], k=-1)
-        return assembly[triangular_indices]
+        return assembly.values[triangular_indices]